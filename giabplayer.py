0bd6df4f69fd4366be8ac7057bdad5ab2054af0f
Nick Bartley <github@nb221.com>
2013-12-08 10:31:08 +0000

    Initial volume reduced to 150

--- giabplayer.py
+++ giabplayer.py
@@ -441,7 +441,7 @@ def Initialize():
 	WLANOn = subprocess.Popen(["sudo", "ifconfig", "wlan0", "up"])
 	
 	#PLAYER_PROCESS = subprocess.Popen(["/usr/bin/vlc", "-I", "dummy", "--volume", "250", "--intf", "telnet"])
-	PLAYER_PROCESS = subprocess.Popen(["/usr/bin/vlc", "-I", "dummy", "--volume", "180", "--intf", "telnet", "--lua-config", "telnet={host='0.0.0.0:4212'}"])
+	PLAYER_PROCESS = subprocess.Popen(["/usr/bin/vlc", "-I", "dummy", "--volume", "150", "--intf", "telnet", "--lua-config", "telnet={host='0.0.0.0:4212'}"])
 	#PLAYER_PROCESS = subprocess.Popen(["/usr/bin/vlc", "-I", "dummy", "--volume", "250", "--intf", "telnet", "--lua-config", "telnet={host='0.0.0.0:4212'}", "--sout", "'#std{access=http,mux=ts,dst=192.168.0.5:1234}'"])
 	
 	
